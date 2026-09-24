d435c7702c44aaa2eb1f1bda0042541566806740
Pali912 <nitindhadwal912@gmail.com>
2026-09-24 23:03:30 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,10 +2,16 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        int count[26]={0};
+        for(char c:s)count[c-'a']++;
+        for(char c:t)count[c-'a']--;
 
+        for(int c: count){
+
+        if(c!=0) return false;
+        }
+
+        return true;
         
     }
 };
